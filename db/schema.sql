8c6d12c0570c43b7f2d6d2448ac8ab3ee462a928
Tyler Reitz <tdreitz@gmail.com>
2017-08-11 21:13:48 -0700

    Add schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+USE bamazon_db;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255) NOT NULL,
+  department_name VARCHAR(255) NOT NULL,
+  price DECIMAL(4,2) NOT NULL,
+  stock_quantity MEDIUMINT NOT NULL,
+  PRIMARY KEY (item_id)
+)
