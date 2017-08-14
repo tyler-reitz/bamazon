32c6b9ed44b9eaf66c4a203919deddb1bb5a22d7
Tyler Reitz <tdreitz@gmail.com>
2017-08-14 16:31:03 -0700

    f*ck 😩

--- bamazon-customer/bamzonCustomer.js
+++ bamazon-customer/bamzonCustomer.js
@@ -1,1 +1,40 @@
-const ascii = require('ascii-art')
+const mysql = require('mysql')
+const inquirer = require('inquirer')
+const { table } = require('ascii-art')
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'G1n&J00$3',
+  database: 'bamazon_db'
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM products', (e,r,f) => {
+  if (e) throw e
+  
+  const q = r.map(d => getObjVals(d))
+    
+  table({ 
+    data: q 
+  }, (params) => {
+    console.log(params)  
+  })
+})
+
+connection.end()
+
+const getObjVals = (d) => {
+  for (var p in d) {
+    if (d.hasOwnProperty(p)) {
+      var element = d[p];
+      d[p] = checkType(element)
+    }
+  }
+  return d
+}
+
+const checkType = (el) => (typeof el === 'number') ? numToString(el) : el
+
+const numToString = (num) => num.toString()
